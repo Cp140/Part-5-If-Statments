767617f2671387ab76173cf8a3ebb858aa8a120c
Cole Palmer <PalmCole551@GoTVDSB.ca>
2022-03-09 10:59:51 -0500

    Add project files.

---
+++ Part 5, If Statments/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Part_5__If_Statments
+{
+    partial class IfStatments
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(273, 90);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(249, 20);
+            this.txtAge.TabIndex = 0;
+            this.txtAge.TextChanged += new System.EventHandler(this.txtAge_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(270, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "                                                                           ";
+            // 
+            // IfStatments
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAge);
+            this.Name = "IfStatments";
+            this.Text = "Part 5, If Statments";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Part 5, If Statments/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Part_5__If_Statments
+{
+    public partial class IfStatments : Form
+    {
+        public IfStatments()
+        {
+            InitializeComponent();
+        }
+
+        private void txtAge_TextChanged(object sender, EventArgs e)
+        {
+            int age = Convert.ToInt32(txtAge.Text);
+            if (age < 5)
+            {
+                
+            }
+            else if (age < 10)
+            {
+                
+            }
+            else if (age < 12)
+            {
+                
+            }
+            else if (age < 18)
+            {
+
+            }
+            else
+            {
+                
+            }
+        }
+    }
+}
